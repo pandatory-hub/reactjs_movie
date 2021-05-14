27fcb0de421fd8703a837f0f4568dc17101decbc
pandatory <20171238@vision.hoseo.edu>
2021-05-14 22:14:15 +0900

    #4.5

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends React.Component {
                 }
             }
         } = await axios.get("http://yts-proxy.now.sh/list_movies.json?sort_by=rating");
-        this.setState({movies, isLoading: false});
+        this.setState({movies, isLoading:false});
     };
 
     componentDidMount() {
